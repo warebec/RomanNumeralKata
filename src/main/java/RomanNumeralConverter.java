e5e33f5b4ff77e6f10ec35ac219d16f2f0930d09
warebec <warebec@gmail.com>
2021-02-14 17:07:51 -0500

    reformatting

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -4,30 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RomanNumeralConverter {
-	
-	static Map<Character, Integer> symbolsMap = new HashMap<>();
-	
-	static {
-		symbolsMap.put('I', 1);
-		symbolsMap.put('V', 5);
-		symbolsMap.put('X', 10);
-		symbolsMap.put('L', 50);
-		symbolsMap.put('C', 100);
-	}
-	
-	public int convert(String given) {
-		int result = symbolsMap.get(given.charAt(0));
-		if (given.length() > 1) {
-			  int second = symbolsMap.get(given.charAt(1));
-			  if (second > result) {
-				  result = second - result;
-				  if (given.length() > 2) {
-					  result += convert(given.substring(2));
-				  }
-			  } else {
-			  	result += convert(given.substring(1));
-			  }
-		}
-		return result;
-	}
+
+    static Map<Character, Integer> symbolsMap = new HashMap<>();
+
+    static {
+        symbolsMap.put('I', 1);
+        symbolsMap.put('V', 5);
+        symbolsMap.put('X', 10);
+        symbolsMap.put('L', 50);
+        symbolsMap.put('C', 100);
+    }
+
+    public int convert(String given) {
+        int result = symbolsMap.get(given.charAt(0));
+        if (given.length() > 1) {
+            int second = symbolsMap.get(given.charAt(1));
+            if (second > result) {
+                result = second - result;
+                if (given.length() > 2) {
+                    result += convert(given.substring(2));
+                }
+            } else {
+                result += convert(given.substring(1));
+            }
+        }
+        return result;
+    }
 }
